8bfbe2871b83110851f160bdeb4f8b7504dab231
Uduak Essien <acushla4real@gmail.com>
2022-04-08 21:05:00 +0100

    Update contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,25 +7,64 @@ import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
 
-  mapping(address => uint256) private userWaves;
+  uint256 private seed;
 
-  constructor() {
+  event NewWave(address indexed from, uint256 timestamp, string message);
+
+  struct Wave {
+    address waver;
+    string message;
+    uint256 timestamp;
+  }
+
+  Wave[] waves;
+
+  mapping(address => uint256) public lastWavedAt;
+
+  constructor() payable {
     console.log("Yo yo, I am a contract and I am smart");
+
+    seed = (block.timestamp + block.difficulty) % 100;
   }
 
-  function wave() public {
+function wave(string memory _message) public {
+
+    require(
+      lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+      "Wait 15m"
+    );
+
+    lastWavedAt[msg.sender] = block.timestamp;
+
     totalWaves += 1;
-    userWaves[msg.sender] += 1;
     console.log("%s has waved!", msg.sender);
-  }
+
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+
+    seed = (block.timestamp + block.difficulty + seed) % 100;
+    console.log("Random # generated: %d", seed);
+
+    if(seed <= 50) {
+      console.log("%s won!", msg.sender);
+
+      uint256 prizeAmount = 0.0001 ether;
+      require(
+          prizeAmount <= address(this).balance,
+          "Trying to withdraw more money than the contract has."
+      );
+      (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+      require(success, "Failed to withdraw money from contract.");
+    }
+
+    emit NewWave(msg.sender, block.timestamp, _message);
+}
 
   function getTotalWaves() public view returns (uint256) {
     console.log("We have %d total waves!", totalWaves);
     return totalWaves;
   }
 
-  function getUserWaves(address user) public view returns (uint256) {
-    console.log("%s has %d waves!", user, userWaves[user]);
-    return userWaves[user];
+  function getAllWaves() public view returns (Wave[] memory) {
+    return waves;
   }
 }
